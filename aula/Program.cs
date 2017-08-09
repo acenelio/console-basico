38d9a829e8c74372458112f84045388550a42cc5
Nelio Alves <acenelio@gmail.com>
2017-08-09 10:56:56 -0300

    Primeiro commit

---
+++ aula/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace aula {
+    class Program {
+        static void Main(string[] args) {
+
+            double x;
+
+            Console.Write("Digite um numero: ");
+            x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Numero digitado = " + x);
+
+            Console.ReadLine();
+        }
+    }
+}
